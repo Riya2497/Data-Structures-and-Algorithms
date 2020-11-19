9174339b54c3b76b4adf386de8914027e99e6f27
Achyut Dubey <achyut.dubey6.4@gmail.com>
2020-11-19 13:23:44 +0530

    Displaying Min heap after k deletions
    Input: (Testcase, n, k, {x_i})
    2
    11
    4
    12 1 21 2 24 23 15 26 4 33 10
    7
    2
    5 2 8 1 4 6 10
    Output:
    1 2 15 4 10 23 21 26 12 33 24
    12 24 15 26 33 23 21
    1 2 6 5 4 8 10
    4 5 6 8 10

---
+++ Heaps/K_Deletions_in_Min-Heap.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a,int *b)
+{
+   int  temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void deleteNode(int arr[], int k, int n)
+{
+    while(k--)
+    {
+        arr[0]=arr[n-1];
+        n--;
+        
+        buildHeap(arr,n);
+    }
+}
+
+void heapify(int arr[],int n,int i)
+{
+    int small = i;
+    int l=2*i+1;
+    int r=2*i+2;
+    
+    if(l<n && arr[l]<arr[small])
+    {
+        small = l;
+    }
+    
+    if(r<n && arr[r]<arr[small])
+    {
+        small = r;
+    }
+    
+    if(small!=i)
+    {
+        swap(&arr[i],&arr[small]);
+        
+        heapify(arr,n,small);
+    }
+}
+
+void buildHeap(int arr[],int n)
+{
+    int si = n/2 -1;
+    
+    for(int i=si;i>=0;i--)
+        heapify(arr,n,i);
+        
+}
+
+void printHeap(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+        
+    printf("\n");
+}
+
+int main() 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n,k;
+	    scanf("%d", &n);
+	    scanf("%d", &k);
+	    int arr[n];
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        scanf("%d",&arr[i]);
+	    }
+	    
+	    buildHeap(arr,n);
+	    printHeap(arr,n);
+	    
+	    deleteNode(arr,k,n);
+	    printHeap(arr,n-k);
+	}
+	return 0;
+}
